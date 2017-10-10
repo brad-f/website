fb7d7dab6d5dbe3ce2ffc8ef27894929b054fe28
Brad Fitzwater <brad.fitzwater@gmail.com>
2017-10-09 20:23:21 -0400

    lint fixes

--- app/components/Title/index.test.js
+++ app/components/Title/index.test.js
@@ -1,9 +1,8 @@
 const React = require('react');
 const enzyme = require('enzyme');
 const Title = require('./index');
-const styles = require('./styles');
 
 test('should render title', () => {
   const title = enzyme.render(<Title />);
   expect(title.text()).toMatch(/brad fitzwater's experiments/);
-})
+});
