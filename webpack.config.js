c25a6811f128421b99ac4aae01fb9250c06d2db9
Brad Fitzwater <brad.fitzwater@gmail.com>
2017-10-29 16:15:17 -0400

    better vendoring

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    vendor: ['react', 'react-dom'],
+    vendor: ['react', 'react-dom', 'react-jss', 'lodash'],
     app: './app/index.js'
   },
   output: {
