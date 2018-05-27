7f53d8ff23361f3bba9dcf797bad1ef9af5b7462
Brad Fitzwater <brad.fitzwater@gmail.com>
2018-05-27 11:33:39 -0400

    delete everything

--- app/components/Title/index.test.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const enzyme = require('enzyme');
-const Title = require('./index');
-
-test('should render title', () => {
-  const title = enzyme.render(<Title />);
-  expect(title.text()).toMatch(/brad fitzwater's experiments/);
-});
--- app/components/screens.js
+++
@@ -1,8 +0,0 @@
-module.exports.medium = classes => ({
-  '@media (min-width: 768px)': classes
-});
-
-
-module.exports.large = classes => ({
-  '@media (min-width: 992px)': classes
-});
