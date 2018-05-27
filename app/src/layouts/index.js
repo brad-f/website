aac7aa772bb00a2b82156da054dcf9a6cbf84a1c
Brad Fitzwater <brad.fitzwater@gmail.com>
2018-05-27 13:18:41 -0400

    added grid

--- app/src/layouts/index.js
+++ app/src/layouts/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import injectSheet from 'react-jss';
 import Helmet from 'react-helmet'
+import PropTypes from 'prop-types'
 
 import Header from '../components/header'
 import './index.css'
 
-const Layout = ({ children, data }) => (
-  <div>
+const styles = {
+  page: {
+    display: 'grid',
+    gridGap: '1rem',
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontFamily: '\'Roboto\', sans-serif'
+  },
+  pageWrapper: {
+    margin: '0 auto',
+    maxWidth: 960,
+    padding: '0px 1.0875rem 1.45rem',
+    paddingTop: 0,
+  }
+};
+
+const Layout = ({ children, data, classes }) => (
+  <div className={classes.page}>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
@@ -15,14 +33,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.headerText} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className={classes.pageWrapper}>
       {children()}
     </div>
   </div>
@@ -32,7 +43,7 @@ Layout.propTypes = {
   children: PropTypes.func,
 }
 
-export default Layout
+export default injectSheet(styles)(Layout)
 
 export const query = graphql`
   query SiteTitleQuery {
