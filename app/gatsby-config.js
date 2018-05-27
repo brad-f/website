84d5cc5ff4c17c2f0a5883a4711be289df045890
Brad Fitzwater <brad.fitzwater@gmail.com>
2018-05-27 12:09:33 -0400

    added headerText

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'bradfitzwater.com',
+    headerText: 'brad fitzwater\'s experiments',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- app/src/layouts/index.js
+++ app/src/layouts/index.js
@@ -14,7 +14,7 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
+    <Header siteTitle={data.site.siteMetadata.headerText} />
     <div
       style={{
         margin: '0 auto',
@@ -39,6 +39,7 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        headerText
       }
     }
   }
