3e05a9748852a7ff1ee37955615acae4877fd632
Brad Fitzwater <brad.fitzwater@gmail.com>
2018-06-04 20:46:04 -0400

    use css modules

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,37 +1,16 @@
 import React from 'react'
-import injectSheet from 'react-jss';
 import Link from 'gatsby-link'
+import styles from './header.module.css';
 
-const styles = {
-  topRow: {
-    gridRow: '1',
-    gridColumn: '1 / 3',
-    textAlign: 'center',
-  },
-  topRowLink: {
-    textDecoration: 'none',
-    color: 'black'
-  },
-  topRowText: {
-    fontSize: '3.0rem'
-  },
-  topRowMutedText: {
-    color: '#636c72'
-  },
-  topRowText: {
-    fontSize: '4.0rem'
-  }
-}
-
-const Header = ({ classes }) => (
-  <div className={classes.topRow}>
-    <h1 className={classes.topRowText}>
-      <Link className={classes.topRowLink} to="/">
+const Header = () => (
+  <div className={styles.topRow}>
+    <h1>
+      <Link to="/">
         brad fitzwater&apos;s
-        <span className={classes.topRowMutedText}> experiments</span>
+        <span className={styles.muted}> experiments</span>
       </Link>
     </h1>
   </div>
 )
 
-export default injectSheet(styles)(Header)
+export default Header
