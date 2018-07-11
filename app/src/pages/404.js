d6c0f0b6c2376cf2b610f5a827482c0d101b587f
Brad Fitzwater <brad.fitzwater@gmail.com>
2018-07-10 22:17:59 -0400

    some cleanup

--- app/src/pages/404.js
+++ app/src/pages/404.js
@@ -3,15 +3,17 @@ import styles from './404.module.css'
 import marlowe from './marlowe.jpg'
 
 const NotFoundPage = () => (
-  <div className={styles.imageWrapper}>
+  <div>
     <h1 className={styles.topRow}>
       <span>404 - NOT FOUND :(</span>
     </h1>
-    <img
-      className={styles.image}
-      alt='marlowe'
-      src={marlowe}
-    />
+    <div className={styles.imageWrapper}>
+      <img
+        className={styles.image}
+        alt='marlowe'
+        src={marlowe}
+      />
+    </div>
   </div>
 )
 
